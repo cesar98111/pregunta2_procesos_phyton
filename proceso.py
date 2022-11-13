dd1beae5719e9a179d5804a1465aa33b52242114
cesar jesus leon martin <cesilius98@gmail.com>
2022-11-13 22:24:09 +0000

    Fix: arreglar problema con la salida de los procesos

--- proceso.py
+++ proceso.py
@@ -6,17 +6,19 @@ def windowProccess():
     numeroPing = input("introduce el numero de ping\n")
     hola = subprocess.run(["ping","-n", numeroPing, ip], capture_output=True)
     response = hola.stdout
-
+    response = response.decode('utf-8')
+    response.split(" ")
     print(response)
     
 
 
 def linuxProccess(): 
     ip = "8.8.8.8"
-    numeroPing = input("introduce el numer\n")
+    numeroPing = input("introduce el numero ping\n")
     hola = subprocess.run(["ping","-c", numeroPing, ip], capture_output=True)
     response = hola.stdout
-
+    response = response.decode('utf-8')
+    response.split(" ")
     print(response)
 
 if __name__ == '__main__':
